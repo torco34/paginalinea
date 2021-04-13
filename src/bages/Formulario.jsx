ee03540f7cc95a1d1626fd2d9a557ca01d5fdb00
torco34 <torcoromaarias@gimail.com>
2021-04-12 23:40:12 -0500

    Eventos enlasados

--- src/bages/Formulario.jsx
+++ src/bages/Formulario.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 
 class Formulario extends React.Component {
+	handleChange = (e) => {
+		console.log({ value: e.target.value });
+	};
+	handleClick = (e) => {
+		console.log("hola botocito");
+	};
 	render() {
 		return (
-			<div>
-				<hi>Nuevo Clientes</hi>
-				<form action=""></form>
+			<div className="container">
+				<div className="row">
+					<div className="col-md-5">
+						<h1>Nuevo Clientes</h1>
+						<form>
+							<div className="form-group">
+								<label htmlFor="">Primer Nombre</label>
+								<input
+									onChange={this.handleChange}
+									className="form-control"
+									type="text"
+									name="firstName"
+								/>
+							</div>
+							<button
+								type="button"
+								onClick={this.handleClick}
+								className="btn card"
+							>
+								Enviar
+							</button>
+						</form>
+					</div>
+					<div className="col-md-6"></div>
+				</div>
 			</div>
 		);
 	}
