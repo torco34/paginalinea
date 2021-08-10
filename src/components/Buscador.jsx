88b8a282cf9c2606aa5d15f6568e8c8641e63562
torco34 <torcoromaarias@gimail.com>
2021-08-10 09:12:36 -0500

    Agregue carpeta router archivo App.js

---
+++ src/components/Buscador.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+class Buscador extends React.Component {
+	render() {
+		return (
+			<div>
+				<form action="">
+					<input type="text" />
+				</form>
+			</div>
+		);
+	}
+}
+export default Buscador;
--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "../assets/estyle/components/Contenedor.css";
+import Buscador from "./Buscador";
 // import Navbar from "../components/Navbar";
 function Contenedo() {
 	// const children = props.children;
 	return (
 		<div>
-			<div className="contenedor"></div>
+			<div className="contenedor">
+				<h2>EL PORTA DE LA MUSICA</h2>
+				<Buscador />
+			</div>
 		</div>
 	);
 }
