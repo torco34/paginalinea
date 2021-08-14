06fe0c81e8414cfca1e8d3997026bd0837d94667
torco34 <torcoromaarias@gimail.com>
2021-08-14 13:43:46 -0500

    BrowserRouter

--- src/routers/App.js
+++ src/routers/App.js
@@ -1,1 +1,9 @@
-App.js;
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+const App = () => {
+	<BrowserRouter>
+		<Route exact path="/" component={Home} />
+		<Route exact path="/login" component={Login} />
+	</BrowserRouter>;
+};
+export default App;
